72f193fb407ca2abcae54a10af9776eb8a653260
Simon Willison <swillison@gmail.com>
2020-04-07 21:21:41 -0700

    Assert now is called with correct options, closes #12

--- tests/test_publish_now.py
+++ tests/test_publish_now.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 from datasette import cli
 from unittest import mock
+import subprocess
 
 
 @mock.patch("shutil.which")
@@ -25,3 +26,20 @@ def test_publish_now_requires_project(mock_which):
         result = runner.invoke(cli.cli, ["publish", "now2", "test.db"])
         assert result.exit_code == 2
         assert "Missing option '--project'" in result.output
+
+
+@mock.patch("shutil.which")
+@mock.patch("datasette_publish_now.run")
+def test_publish_now(mock_run, mock_which):
+    mock_which.return_value = True
+    mock_run.return_value = mock.Mock(0)
+    mock_run.return_value.stdout = b"https://demo.example.com/"
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        open("test.db", "w").write("data")
+        runner.invoke(
+            cli.cli, ["publish", "now2", "test.db", "--project", "foo"],
+        )
+        mock_run.assert_has_calls(
+            [mock.call(["now", "--confirm", "--no-clipboard", "--prod"]),]
+        )
